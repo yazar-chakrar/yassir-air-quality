0fc0c193b2f85351d0b6a854c851413c1ded764a
yazarchakrar <hm_chakrar@esi.dz>
2022-10-26 03:26:18 +0100

    aqi-model

--- api/models/aqi.js
+++ api/models/aqi.js
@@ -39,16 +39,4 @@ const aqiShema = mongoose.Schema({
 
 const Aqi = mongoose.model('Aqis', aqiShema);
 
-function validate(aqi){
-    const schema = Joi.object({
-        ts: Joi.date.required(),
-        mainus: Joi.string().required(),
-        maincn: Joi.string().required(),
-        aqius: Joi.number().min(0).required(),
-        aqicn: Joi.number().min(0).required(),
-    });
-    return schema.validate(aqi);
-}
-
 exports.Aqi = Aqi;
-exports.validate = validate;
